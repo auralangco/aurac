8d27d55f8acbbb433e1d1e96d9c1bd94e1f6ba19
OJarrisonn <j.h.m.t.v.10@gmail.com>
2024-06-20 13:59:49 -0300

    ir/program: adds includes to the program and the `core.h`
    `includes` adds `#include` for C header files during compilation

--- src/ir/program.rs
+++ src/ir/program.rs
@@ -6,6 +6,9 @@ use super::{Atom, Compilable, Literal};
 /// 
 /// The program should contain enough information to produce a correct C program.
 pub struct Program {
+    /// The list of C header files to include in the compilation.
+    pub includes: Vec<String>,
+
     /// Atoms are defined globally in the program.
     /// A total of 2^64 atoms can be defined in a program.
     pub atoms: Vec<Atom>,
@@ -18,9 +21,14 @@ impl Compilable for Program {
     fn compile(&self) -> String {
         let mut code = String::new();
 
-        code.push_str("#include <stdbool.h>\n");
+        code.push_str(include_str!("core.h"));
 
-        code.push_str("// Section atoms\n");
+        code.push_str("\n// Section includes\n");
+        for include in &self.includes {
+            code.push_str(&format!("#include \"{}\"\n", include));
+        }
+
+        code.push_str("\n// Section atoms\n");
         for (i, atom) in self.atoms.iter().enumerate() {
             code.push_str(&format!("#define {} {}\n", atom.repr, i));
         }
@@ -44,6 +52,7 @@ mod tests {
     #[test]
     fn test_program_compile() {
         let program = Program {
+            includes: vec!["stdio.h".into()],
             atoms: vec![Atom::from_identifier("a"), Atom::from_identifier("b")],
             program: vec![
                 Literal::Int(42),
